a7a5ccbc638995e49b7cac3b6a914c4aa1a7aacf
Kira Tilcock <kiratilcock@hotmail.com>
2020-05-03 14:02:01 -0700

    Increase loading time

--- assets/js/loading.js
+++ assets/js/loading.js
@@ -1,5 +1,5 @@
 function onReady(callback) {
-    var intervalID = window.setInterval(checkReady, 1000);
+    var intervalID = window.setInterval(checkReady, 2500);
 
     function checkReady() {
         if (document.getElementsByTagName('body')[0] !== undefined) {
