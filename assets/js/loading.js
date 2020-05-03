51d5a265bf7b5bae722480d572becf9f345b84f5
Kira Tilcock <kiratilcock@hotmail.com>
2020-05-03 14:34:37 -0700

    loading screen tweaks

--- assets/js/loading.js
+++ assets/js/loading.js
@@ -1,5 +1,5 @@
 function onReady(callback) {
-    var intervalID = window.setInterval(checkReady, 2600);
+    var intervalID = window.setInterval(checkReady, 3000);
 
     function checkReady() {
         if (document.getElementsByTagName('body')[0] !== undefined) {
