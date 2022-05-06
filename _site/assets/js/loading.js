b5235868530e7924e26988bee6c2d16f16cf5759
Eamon Murphy <eamonmurphy@outlook.com>
2022-05-06 14:42:03 +0200

    Switch to Jekyll & add blog

---
+++ _site/assets/js/loading.js
@@ -0,0 +1,19 @@
+function onReady(callback) {
+    var intervalID = window.setInterval(checkReady, 3200);
+
+    function checkReady() {
+        if (document.getElementsByTagName('body')[0] !== undefined) {
+            window.clearInterval(intervalID);
+            callback.call(this);
+        }
+    }
+}
+
+function show(id, value) {
+    document.getElementById(id).style.display = value ? 'block' : 'none';
+}
+
+onReady(function () {
+    show('page', true);
+    show('loading', false);
+});
